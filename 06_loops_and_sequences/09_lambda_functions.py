922a2682d0d1cd368196aaa3d493ae21c56178e9
victorke485 <victorvickuat@gmail.com>
2026-02-06 15:31:51 +0300

    update: lambda function

--- 06_loops_and_sequences/09_lambda_functions.py
+++ 06_loops_and_sequences/09_lambda_functions.py
@@ -3,3 +3,9 @@
 numbers = [2, 23, 22, 10]
 even_numbers = list(filter(lambda x: x % 2 ==0, numbers))
 print(even_numbers)
+
+add = lambda x, y: x + y
+print(add(2, 4))
+
+odd_numbers = list(filter(lambda x: x % 2 == 1, numbers))
+print(odd_numbers)
