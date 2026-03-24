f682722a8d1096ddd5c51711ec05b5a8996e83bb
victorke485 <victorvickuat@gmail.com>
2026-03-24 16:25:22 +0300

    Update: ceasr cipher project

--- 00_projects/02_ceasar_cipher/main.py
+++ 00_projects/02_ceasar_cipher/main.py
@@ -3,38 +3,33 @@ from string import ascii_lowercase
 
 print(logo)
 
-alphabet = list(ascii_lowercase)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
-message = input("Type your message: ").lower()
-shift = int(input("Type the shift number: "))
-
-def encrypt(shift, message):
-    shifted_alphabets = alphabet[shift:]
-    shifted_alphabets.extend(alphabet[:shift])
-    encrypted_message = []
-    for letter in message:
-        encrypted_message.append(shifted_alphabets[alphabet.index(letter)])
-    return "".join(encrypted_message)
-
-def decrypt(shift, message):
-    shifted_alphabets = alphabet[shift:]
-    shifted_alphabets.extend(alphabet[:shift])
-    decrypted_message = []
-    for letter in message:
-        decrypted_message.append( alphabet[shifted_alphabets.index(letter)] )
-    return "".join(decrypted_message)
-
-print(encrypt(100, "hello"))
-# print(decrypt(10, "rovvy"))
-print(len(alphabet))
-
-if shift > len(alphabet):
-    print(f"Enter shift number which is less than {len(alphabet)}")
-elif direction != "encode" or direction != "decode":
-    print("Enter a valid operation. eg 'encode' or 'decode")
-else:
-    if direction == "encode":
-        encrypt(shift, message)
+def encrypt_decrypt(shift, direction, message):
+    cipher = ""
+    if direction == "encode" or direction == "decode":    
+        for i in message:
+            if direction == "encode":
+                shifted_index = alphabet.index(i) + shift
+            else:
+                shifted_index = alphabet.index(i) - shift
+            shifted_index %= len(alphabet)
+            cipher+=alphabet[shifted_index]
+        print(f"Your {direction}d message is {cipher}")
     else:
-        decrypt(shift, message)
+        print("Enter valid option.")
 
+
+exit = False
+
+while not exit:
+    alphabet = list(ascii_lowercase)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+    message = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
+
+    encrypt_decrypt(shift, direction, message)
+
+    proceed_choice = input("Proceed with the ceaser cipher? (Type yes to proceed and no to quit): ")
+    if proceed_choice == "yes":
+        exit = False
+    else:
+        exit = True
