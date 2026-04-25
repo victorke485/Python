1b3292101eeef382d6c6fa7b7e3b0fe5c20b32d2
victorke485 <victorvickuat@gmail.com>
2026-04-25 12:02:23 +0300

    add: password manager project

---
+++ 00_projects/21_password_manager/main.py
@@ -0,0 +1,79 @@
+from tkinter import *
+from tkinter import messagebox
+import string
+import random
+import pyperclip
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    password = ""
+    password += "".join([random.choice(string.ascii_uppercase) for _ in range(4)])
+    password += "".join([random.choice(string.ascii_lowercase) for _ in range(4)])
+    password += "".join([random.choice(string.digits) for _ in range(4)])
+    password += "".join([random.choice(string.punctuation) for _ in range(4)])
+    password = "".join(random.sample(password, len(password)))
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any field empty!")
+    else:
+        is_ok = messagebox.askokcancel(
+            title=website,
+            message=f"These are the details entered: \nEmail: {email}\nPassword: {password}\nIs it ok to save?",
+        )
+
+        if is_ok:
+            with open("00_projects/21_password_manager/data.txt", "a") as f:
+                f.write(f"{website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50, bg="white")
+
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+logo_img = PhotoImage(file="00_projects/21_password_manager/logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:", bg="white")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
+email_label = Label(text="Email/Username:", bg="white")
+email_label.grid(row=2, column=0)
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:", bg="white")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_password_button = Button(
+    text="Generate Password", bg="white", highlightthickness=0, command=generate_password
+)
+generate_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36, bg="white", command=save_password)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
