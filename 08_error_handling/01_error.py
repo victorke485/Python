28963b065338c832991489c95008dcb8d3201a3d
victorke485 <victorvickuat@gmail.com>
2026-04-26 14:29:03 +0300

    feat: try, except, else, finally

--- 08_error_handling/01_error.py
+++ 08_error_handling/01_error.py
@@ -1,11 +1,14 @@
 # Types of error: TypeError, ValueError
 
+
 def average(values):
     try:
         return sum(values) / len(values)
     except:
         print("Please provide an iterable data structure")
-print(average("we"))
+
+
+# print(average("we"))
 print(average([12, 23, 43]))
 
 
@@ -16,5 +19,25 @@ def average_2(values):
     else:
         raise TypeError("Please provide an iterable data structure")
 
-print(average_2("we"))
+
+# print(average_2("we"))
 print(average_2([12, 23, 43]))
+
+# try except
+try:
+    a_dictionary = {"name": "victor"}
+    print(a_dictionary["score"])
+except KeyError:
+    print("That key does not exist")
+
+# try, except, else, finally
+try:
+    file = open("sfile1.txt")
+except FileNotFoundError as error_message:
+    print(f"An error occured: {error_message}")
+    file = open("sfile1.txt", "w")
+else: # gets execured if no error occured
+    print("No error occured")
+finally:
+    file.close()
+    print("File was closed")
