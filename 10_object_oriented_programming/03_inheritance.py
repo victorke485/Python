83b1317dd1e6b5540d02f09b8bbaacecefe14591
victorke485 <victorvickuat@gmail.com>
2026-05-29 21:08:58 +0300

    feat: add inheritance, polymorphism, abstraction

---
+++ 10_object_oriented_programming/03_inheritance.py
@@ -0,0 +1,20 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+    
+    def breath(self):
+        print("Inhale, exhale")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breath(self):
+        super().breath()
+        print("doing this underwater")
+
+    def swim(self):
+        print("moving in water.")
+
+nemo = Fish()
+nemo.breath()
